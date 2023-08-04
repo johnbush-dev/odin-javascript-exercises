51dffe22ddd81c095a46cda5d16fc8409cbe3621
johnbush-dev <jm-bush-developer@outlook.com>
2023-08-04 15:45:11 +0100

    Odin exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(userInput, repeatNumber) {
+    if (repeatNumber < 0) return "ERROR";
+    let finalString = "";
+    for (let i = 0; i < repeatNumber; i++) {
+        finalString += userInput;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
