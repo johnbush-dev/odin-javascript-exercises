a9202d246cdf2a7df9c1b3daed4d1dcf760e4bc0
johnbush-dev <jm-bush-developer@outlook.com>
2023-08-04 15:56:48 +0100

    Odin exercise 4 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(userInput) {
+    let newString = "";
+    for (i = userInput.length - 1; i >= 0; i--) {
+        newString += userInput[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
